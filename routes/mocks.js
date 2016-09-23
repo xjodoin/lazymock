ab017b66a58050ec364746b709033f55a2e45400
Xavier Jodoin <xavier@jodoin.me>
2016-09-23 10:46:36 -0400

    first commit

---
+++ routes/mocks.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var _ = require('lodash');
+var router = express.Router();
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+/* GET mocks listing. */
+router.post('/1.0/tax/get', function(req, res, next) {
+    var amount = req.body.Lines[0].Amount;
+    var taxes = [{
+        TaxName: 'CANADA GST/TPS',
+        Rate: 0.05,
+        Tax: 0
+    }, {
+        TaxName: 'QUEBEC QST/TVQ',
+        Rate: 0.09975,
+        Tax: 0
+    }];
+
+    var avatax = {
+        TaxSummary: taxes
+    };
+
+    _.each(taxes, function(tax) {
+        tax.Tax = roundToTwo(amount * tax.Rate);
+    });
+
+    res.send(avatax);
+});
+
+module.exports = router;
